f5d43193af3c0483f40639c68ee11d86b8ab0f5d
Master Kwoth <master.kwoth@gmail.com>
2017-12-30 22:44:51 +0700

    Permissions back, testing further

--- NadekoBot.Core/Common/TypeReaders/BotCommandTypeReader.cs
+++ NadekoBot.Core/Common/TypeReaders/BotCommandTypeReader.cs
@@ -34,46 +34,46 @@ namespace NadekoBot.Common.TypeReaders
         }
     }
     //todo 999 restore
-    //public class CommandOrCrTypeReader : NadekoTypeReader<CommandOrCrInfo>
-    //{
-    //    private readonly DiscordSocketClient _client;
-    //    private readonly CommandService _cmds;
-    //    public CommandOrCrTypeReader(DiscordSocketClient client, CommandService cmds) : base(client, cmds)
-    //    {
-    //        _client = client;
-    //        _cmds = cmds;
-    //    }
+    public class CommandOrCrTypeReader : NadekoTypeReader<CommandOrCrInfo>
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _cmds;
+        public CommandOrCrTypeReader(DiscordSocketClient client, CommandService cmds) : base(client, cmds)
+        {
+            _client = client;
+            _cmds = cmds;
+        }
 
-    //    public override async Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider services)
-    //    {
-    //        input = input.ToUpperInvariant();
+        public override async Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider services)
+        {
+            input = input.ToUpperInvariant();
 
-    //        var _crs = ((INServiceProvider)services).GetService<CustomReactionsService>();
+            //var _crs = ((INServiceProvider)services).GetService<CustomReactionsService>();
 
-    //        if (_crs.GlobalReactions.Any(x => x.Trigger.ToUpperInvariant() == input))
-    //        {
-    //            return TypeReaderResult.FromSuccess(new CommandOrCrInfo(input));
-    //        }
-    //        var guild = context.Guild;
-    //        if (guild != null)
-    //        {
-    //            if (_crs.GuildReactions.TryGetValue(guild.Id, out var crs))
-    //            {
-    //                if (crs.Concat(_crs.GlobalReactions).Any(x => x.Trigger.ToUpperInvariant() == input))
-    //                {
-    //                    return TypeReaderResult.FromSuccess(new CommandOrCrInfo(input));
-    //                }
-    //            }
-    //        }
+            //if (_crs.GlobalReactions.Any(x => x.Trigger.ToUpperInvariant() == input))
+            //{
+            //    return TypeReaderResult.FromSuccess(new CommandOrCrInfo(input));
+            //}
+            //var guild = context.Guild;
+            //if (guild != null)
+            //{
+            //    if (_crs.GuildReactions.TryGetValue(guild.Id, out var crs))
+            //    {
+            //        if (crs.Concat(_crs.GlobalReactions).Any(x => x.Trigger.ToUpperInvariant() == input))
+            //        {
+            //            return TypeReaderResult.FromSuccess(new CommandOrCrInfo(input));
+            //        }
+            //    }
+            //}
 
-    //        var cmd = await new CommandTypeReader(_client, _cmds).ReadAsync(context, input, services);
-    //        if (cmd.IsSuccess)
-    //        {
-    //            return TypeReaderResult.FromSuccess(new CommandOrCrInfo(((CommandInfo)cmd.Values.First().Value).Name));
-    //        }
-    //        return TypeReaderResult.FromError(CommandError.ParseFailed, "No such command or cr found.");
-    //    }
-    //}
+            var cmd = await new CommandTypeReader(_client, _cmds).ReadAsync(context, input, services);
+            if (cmd.IsSuccess)
+            {
+                return TypeReaderResult.FromSuccess(new CommandOrCrInfo(((CommandInfo)cmd.Values.First().Value).Name));
+            }
+            return TypeReaderResult.FromError(CommandError.ParseFailed, "No such command or cr found.");
+        }
+    }
 
     public class CommandOrCrInfo
     {
